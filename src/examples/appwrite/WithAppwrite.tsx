1e31752c5d53c157719c917fbb7f8255c93f866c
BeltEBelt <155157292+beltebelt@users.noreply.github.com>
2025-09-27 12:20:11 +0530

    Update WithAppwrite.tsx

--- src/examples/appwrite/WithAppwrite.tsx
+++ src/examples/appwrite/WithAppwrite.tsx
@@ -1,4 +1,4 @@
-import { ExcalidrawApp, StorageProvider } from "excalidraw-with-storage";
+import { ExcalidrawApp, type StorageProvider } from "excalidraw-with-storage";
 import { Client, Databases, Storage, ID, Query } from "appwrite";
 
 // Crude workaround since appwrite restricts fileId to 36 characters
